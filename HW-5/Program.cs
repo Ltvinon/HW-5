8be01b9953fb76b51e1476d601de63805610e412
Andrii <ltvinon@gmil.com>
2023-02-07 14:38:42 +0200

    Add project files.

---
+++ HW-5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Привіт привіт, це курс курс по дотнету дотнету.";
+
+        string pattern = @"\b(\w+)\b\s+(\b\1\b)";
+
+        Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+        string result = regex.Replace(text, "$1");
+
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine(result);
+    }
+}
